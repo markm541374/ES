c8dc437ab593005bb76eb5905afe4534c4f91290
markm541374 <markm@robots.ox.ac.uk>
2015-02-04 11:58:08 +0000

    moved GPep to class in searate file

---
+++ GPep.py
@@ -0,0 +1,137 @@
+#Gaussian process with expectation propagation on inequality constraints
+
+import scipy as sp
+from functools import partial
+from matplotlib import pyplot as plt
+import scipy.stats as sps
+import scipy.linalg as spl
+import GPd
+
+def catObs(O):
+    return GPd.catObs(O)
+
+def plot1(g,llimit,ulimit):
+    return GPd.plot1(g,llimit,ulimit)
+
+def gen_sqexp_k_d(theta):
+    k = partial(GPd.sqexp_k_d,theta)
+    return k
+
+def PhiR(x):
+    return sps.norm.pdf(x)/sps.norm.cdf(x)
+
+class GPcore:
+    def __init__(self,X_c,Y_c,S_c,D_c,X_z,D_z,I_z,G_z,kf):
+       #X,Y,S,D_c are the x,y,var and derivative that have been observed
+       #X,I,G_z are the x,y and sign of inequality constraints
+       self.Xc=X_c
+       self.Yc=Y_c
+       self.Sc=S_c
+       self.Dc=D_c
+       self.Xz=X_z
+       self.Dz=D_z
+       self.Iz=I_z
+       self.Gz=G_z
+       self.kf=kf
+       self.n_z=len(G_z)
+       self.n_c=len(D_z)
+
+       self.nloops=10
+       self.invalidflag=True
+       return
+
+    def runEP(self,plot=False):
+       g=GPd.GPcore(self.Xc,self.Yc,self.Sc,self.Dc,self.kf)
+       #start by making the full inference at the inequality locations
+       [m0,V0]=g.infer_full(self.Xz,self.Dz)
+       V0Im0=spl.cho_solve(spl.cho_factor(V0),m0)
+       V0I=V0.I #------------------------------------------------------------------explicit inverse, not good
+       #create the ep observations
+       yt=sp.matrix(sp.zeros([self.n_z,1]))
+       St=10**10*sp.matrix(sp.ones([self.n_z,1]))
+
+       ytR=yt.copy()
+       StR=St.copy()
+       m_R=[[],[]]
+       v_R=[[],[]]
+
+       for it in xrange(self.nloops):
+            for i in xrange(self.n_z):
+               #update the inference at z with the ep observations
+               StIyt=sp.matrix(sp.zeros([self.n_z,1]))
+               StI=sp.matrix(sp.zeros([self.n_z,self.n_z]))
+               VtIV0I=V0I.copy()
+               #print V0I
+               for j in xrange(self.n_z):
+                  StIyt[j,0]=yt[j,0]/float(St[j,0])
+                  VtIV0I[j,j]+=1./float(St[j,0])
+
+               Vp=VtIV0I.I.copy()
+               mp=Vp*(StIyt+V0Im0)
+
+            #get the inference at the specific inequality currently being updates
+
+        
+               v_ = 1/(1/(Vp[i,i]) - 1/(St[i,0]))
+               m_ = v_*(mp[i,0]/Vp[i,i]-yt[i,0]/St[i,0])
+        
+               m_R[i].append(m_)
+               v_R[i].append(v_)
+            #find the new ep obs
+
+               alpha = (m_+((-1)**self.Gz[i])*self.Iz[i,0])/(sp.sqrt(v_+10**-10))
+        
+               pr = PhiR(alpha)
+               if sp.isnan(pr):
+                   pr=-alpha
+               beta = pr*(pr+alpha)/(v_+10**-10)
+               kappa = ((-1)**self.Gz[i] )*(pr+alpha)/(sp.sqrt(v_+10**-10))
+
+               #replace with the new ep obs
+               yt[i,0]=m_-1./kappa
+               St[i,0]=1./beta - v_
+            #print yt
+            #print St
+            ytR=sp.hstack([ytR,yt])
+            StR=sp.hstack([StR,St])
+       if plot:
+          plt.figure()
+          sign=sp.zeros([self.n_z,self.nloops])
+          for i in xrange(self.n_z):
+              for  j in xrange(self.nloops+1):
+                 if StR[i,j]<=0:
+                    sign[i,j]=1
+                 StR[i,j]=sp.log10(StR[i,j])
+
+          for j in xrange(self.n_z):
+             plt.plot(sp.array(StR[j,:]).flatten())
+
+
+       Yz=yt.copy()
+       #Dz already defined
+       Sz=St.copy()
+
+       [Xep,Yep,Sep,Dep]=GPd.catObs([[self.Xc,self.Yc,self.Sc,self.Dc],[self.Xz,Yz,Sz,self.Dz]])
+
+       self.gep=GPd.GPcore(Xep,Yep,Sep,Dep,self.kf)
+       self.invalidflag=False
+       return
+
+    def infer_m(self,X_i,D_i):
+        if self.invalidflag:
+           self.runEP()
+        return self.gep.infer_m(X_i,D_i)
+
+    def infer_full(self,X_i,D_i):
+        if self.invalidflag:
+           self.runEP()
+
+        return self.gep.infer_full(X_i,D_i)
+    
+    def infer_diag(self,X_i,D_i):
+        if self.invalidflag:
+           self.runEP()
+        return self.gep.infer_diag(X_i,D_i)
+
+
+
