826f2153630054a6114adbee049874cfbc6b7b41
markm541374 <mark.mcleod.17@googlemail.com>
2015-08-20 21:12:54 +0100

    cleanup

--- paras/test3.py
+++ paras/test3.py
@@ -9,21 +9,21 @@ import scipy as sp
 
 optpara=dict()
 optpara['D']=3
-optpara['nHYPsamples']=6
-optpara['nHYPmargin']=1.8
+optpara['nHYPsamples']=8
+optpara['nHYPmargin']=1.5
 optpara['HYPsearchLow'] = [-2, -2, -2, -2]
 optpara['HYPsearchHigh'] = [2, 2, 2, 2]
-optpara['HYPMLEsearchn'] = 1200
+optpara['HYPMLEsearchn'] = 800
 optpara['HYPsamSigma'] = 0.05
 optpara['HYPsamBurn'] = 12
 optpara['ENTnsam'] = 400
 optpara['ENTsamQ'] = 'method2'
 optpara['ENTzeroprecision'] = 10**-6
-optpara['ENTsearchn'] = 2000
-optpara['IRsearchn'] = 2000
+optpara['ENTsearchn'] = 500
+optpara['IRsearchn'] = 1000
 optpara['searchmethod']=['EIMLE','EIFB','fixs','discretes'][3]
-optpara['slist'] = sp.logspace(1,-6,20)
-optpara['ulist'] = [1./(x**0.1) for x in optpara['slist']]
+optpara['slist'] = sp.logspace(2,-4,20)
+optpara['ulist'] = [1./(x**0.6) for x in optpara['slist']]
 optpara['fixs'] = 0.001
 from scipy import NaN as nan
 optpara['obstype'] = [nan]
@@ -37,7 +37,7 @@ optpara['covtype'] = 'sqexp'
 #prior over hyp, mean and var on logscale, output then axes
 optpara['prior'] = [[0.,2.*2],[0., 2.**2], [0., 2.**2], [0., 2.**2]]
 optpara['inittype']='rand'
-optpara['nrand']=20
+optpara['nrand']=5
 optpara['boundregion']=0.99
 
 
@@ -45,8 +45,8 @@ optpara['boundregion']=0.99
 objf=dict()
 objf['type']='drawfromcov'
 objf['covgen']='sqexp'
-objf['D']=optpara['D']
-objf['hyp']=[1.,0.5, 0.4,0.6]
+objf['D']=3
+objf['hyp']=[1.,0.2, 0.1, 0.2]
 #always scale to [-1.1]^D
 #objf['lower']=[-1.]
 #objf['upper']=[1.]
@@ -55,4 +55,4 @@ objf['hyp']=[1.,0.5, 0.4,0.6]
 
 runs=dict()
 runs['nopts']=1
-runs['nsteps']= 25
+runs['nsteps']=5
